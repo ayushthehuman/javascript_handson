21b129f957f1f9f7daf89044e28dd2bba6914480
AyushKumarSahu <ayushkumarsahu@cedcoss.com>
2022-02-05 11:56:04 +0530

    Decrementation-in-JS

--- src/javascript_handson.js
+++ src/javascript_handson.js
@@ -64,4 +64,9 @@ const quotient = 66 / 33;
 let myVar = 87;
 // Only change code below this line
 myVar++;
+/* Decrement in JS. */
+let myVar = 11;
+
+// Only change code below this line
+myVar--;
 
