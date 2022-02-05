074327716ba0ffa88c85a861fb11423e62bba051
AyushKumarSahu <ayushkumarsahu@cedcoss.com>
2022-02-05 11:16:15 +0530

    uninitilalized-var

--- src/javascript_handson.js
+++ src/javascript_handson.js
@@ -20,4 +20,17 @@ var a=9;
 /* Declare String Variables*/
 var myFirstName = "a";
 var myLastName =  "s";
+/* Understanding Uninitialized Variables */
+// Only change code below this line
+var a;
+var b;
+var c;
+a=5;
+b=10;
+c="I am a"
+// Only change code above this line
+
+a = a + 1;
+b = b + 5;
+c = c + " String!";
 
