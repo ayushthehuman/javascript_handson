b672c100d8faba9838638772f74451f2b7bb37d2
AyushKumarSahu <ayushkumarsahu@cedcoss.com>
2022-02-05 13:53:56 +0530

    Access-arr-Indexes

--- src/javascript_handson.js
+++ src/javascript_handson.js
@@ -187,3 +187,8 @@ const myArray = ["Apple" , 45];
 /* Storing multiple array using JS */
 // Only change code below this line
 const myArray = [[], []];
+/* Access Array Data with Indexes */
+const myArray = [50, 60, 70];
+myArray[0];
+const myData=myArray[0];
+
