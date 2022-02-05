0331852ee695a54b486d7b0681e82723ee66f96a
AyushKumarSahu <ayushkumarsahu@cedcoss.com>
2022-02-05 13:05:23 +0530

    Bracket-notation-find-first-character

--- src/javascript_handson.js
+++ src/javascript_handson.js
@@ -140,3 +140,10 @@ const lastName = "Lovelace";
 
 // Only change code below this line
 lastNameLength = lastName.length;
+/* Use Bracket Notation to Find the First Character in a String */
+// Setup
+let firstLetterOfLastName = "";
+const lastName = "Lovelace";
+
+// Only change code below this line
+firstLetterOfLastName = lastName[0]; // Change this line
