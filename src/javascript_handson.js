3256204c12480c13cdb00424bbc71b546b1c15e3
AyushKumarSahu <ayushkumarsahu@cedcoss.com>
2022-02-05 15:31:34 +0530

    Comparison-Equal-operator

--- src/javascript_handson.js
+++ src/javascript_handson.js
@@ -358,4 +358,13 @@ function nextInLine(arr, item) {
   
     // Only change code above this line.
   }
+  /* Comparison with the Equality Operator */
+  // Setup
+function testEqual(val) {
+    if (val==12) { // Change this line
+      return "Equal";
+    }
+    return "Not Equal";
+  }
+  testEqual(10);
   
