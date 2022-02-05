b628015c68cce49b095f514e888d26df92ae17d8
AyushKumarSahu <ayushkumarsahu@cedcoss.com>
2022-02-05 12:43:08 +0530

    contractiong-string-with-plus-op

--- src/javascript_handson.js
+++ src/javascript_handson.js
@@ -119,3 +119,5 @@ const myStr = "I am a \"double quoted\" string inside \"double quotes\"."; // Ch
 const myStr = '<a href="http://www.example.com" target="_blank">Link</a>';
 /* Escape Sequences in Strings */
 const myStr = "FirstLine\n\t\\SecondLine\nThirdLine"; // Change this line
+/* Concatenating Strings with Plus Operator */
+const myStr =  "This is the start. " + "This is the end."; // Change this line
