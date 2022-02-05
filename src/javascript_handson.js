f6ae96e9d78ba6067317fc5e2d013e3e1256555e
AyushKumarSahu <ayushkumarsahu@cedcoss.com>
2022-02-05 11:03:59 +0530

    assign-val-onevar-anothervar

--- src/javascript_handson.js
+++ src/javascript_handson.js
@@ -8,6 +8,13 @@ var myName;
 var a;
 a=7;
 // Only change code below this line
+/* Assigning the Value of One Variable to Another */
+// Setup
+var a;
+a = 7;
+var b;
+b=a;
+// Only change code below this line
 
 
 
