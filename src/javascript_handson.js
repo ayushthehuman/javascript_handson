34bac2f86de4025c7b5f6f2c2162534d9d72f8f8
AyushKumarSahu <ayushkumarsahu@cedcoss.com>
2022-02-05 12:15:29 +0530

    Compound-Assingment-Augmented-Multiply

--- src/javascript_handson.js
+++ src/javascript_handson.js
@@ -95,4 +95,13 @@ let c = 3;
 a -= 6;
 b -= 15;
 c -= 1 ;
+/* Compound Assignment With Augmented Multiply */
+let a = 5;
+let b = 12;
+let c = 4.6;
+
+// Only change code below this line
+a *= 5;
+b *= 3;
+c *= 10;
 
