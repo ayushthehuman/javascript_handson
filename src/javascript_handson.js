ea149686377224ac3c217834f54ea1e1b3aa712f
AyushKumarSahu <ayushkumarsahu@cedcoss.com>
2022-02-05 13:43:55 +0530

    world-blank

--- src/javascript_handson.js
+++ src/javascript_handson.js
@@ -172,3 +172,12 @@ const lastName = "Lovelace";
 
 // Only change code below this line
 const secondToLastLetterOfLastName = lastName[lastName.length -2]; // Change this line
+/* Word Blank */
+const myNoun = "dog";
+const myAdjective = "big";
+const myVerb = "ran";
+const myAdverb = "quickly";
+
+// Only change code below this line
+const wordBlanks =  "The " + myAdjective + " " + myNoun + " " + myVerb + " " + myAdverb + "."; // Change this line
+// Only change code above this line
