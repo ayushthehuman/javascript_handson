dc5bbc8c88a4b7e50e5481d6c6a9be24d1c35de0
AyushKumarSahu <ayushkumarsahu@cedcoss.com>
2022-02-05 13:56:25 +0530

    Modify-Array

--- src/javascript_handson.js
+++ src/javascript_handson.js
@@ -191,4 +191,9 @@ const myArray = [[], []];
 const myArray = [50, 60, 70];
 myArray[0];
 const myData=myArray[0];
+/* Modify Array Data With Indexes */
+// Setup
+const myArray = [18, 64, 99];
 
+// Only change code below this line
+myArray[0]=45;
