f97b00623fdd2527db9a4bba89f10eadca962225
AyushKumarSahu <ayushkumarsahu@cedcoss.com>
2022-02-05 15:27:10 +0530

    Conditional-logic

--- src/javascript_handson.js
+++ src/javascript_handson.js
@@ -348,3 +348,14 @@ function nextInLine(arr, item) {
   
     // Only change code above this line
   }
+  /* Use Conditional Logic with If Statements */
+  function trueOrFalse(wasThatTrue) {
+    // Only change code below this line
+    if (wasThatTrue) {
+      return "Yes, that was true";
+    }
+    return "No, that was false";
+  
+    // Only change code above this line.
+  }
+  
