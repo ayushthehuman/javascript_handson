24cc41dbd08dd419d98cc9512024a0b0c2614120
AyushKumarSahu <ayushkumarsahu@cedcoss.com>
2022-02-05 15:57:06 +0530

    comparing-wit-logical-op

--- src/javascript_handson.js
+++ src/javascript_handson.js
@@ -476,7 +476,19 @@ function testLogicalAnd(val) {
   }
   
   testLogicalAnd(10);
-
+/*Comparisons with the Logical Or Operator*/
+function testLogicalOr(val) {
+    // Only change code below this line
+  
+    if (val>20||val<10) {
+      return "Outside";
+    }
+  
+    // Only change code above this line
+    return "Inside";
+  }
+  
+  testLogicalOr(15);
 
 
 
