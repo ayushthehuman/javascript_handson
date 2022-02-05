dcf67d00e570165b42b7d50083e0a93ca13e3cc3
AyushKumarSahu <ayushkumarsahu@cedcoss.com>
2022-02-05 14:48:56 +0530

    local-scope-func

--- src/javascript_handson.js
+++ src/javascript_handson.js
@@ -275,3 +275,14 @@ function fun2() {
   }
   console.log(output);
 }
+/* Local Scope and Functions */
+function myLocalScope() {
+    // Only change code below this line
+  var myVar;
+    console.log('inside myLocalScope', myVar);
+  }
+  myLocalScope();
+  
+  // Run and check the console
+  // myVar is not defined outside of myLocalScope
+  console.log('outside myLocalScope', myVar);
